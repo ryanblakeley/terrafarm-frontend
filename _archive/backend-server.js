a513fcc3f52f45ee139a3b17f8e24dd5b870c22a
Ryan Blakeley <rbblakeley@gmail.com>
2016-03-10 10:30:53 -0700

    Deleted digital ocean starter files

--- _archive/backend-server.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Backendend server says:\n Hello World\n Coming soon\n');
-}).listen(8080, '10.134.12.160');
-console.log('Server running at http://10.134.12.160:8080/');
--- _archive/frontend-server.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Frontend server says:\nHello World\nComing soon\n');
-}).listen(3000, '10.134.12.160');
-console.log('Server running at http://10.134.12.160:3000/');
