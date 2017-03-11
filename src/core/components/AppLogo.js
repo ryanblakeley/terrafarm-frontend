a99eed1e09b1757d4376cea35233e68d6953e3d9
Ryan Blakeley <rbblakeley@gmail.com>
2017-03-11 14:24:59 -0700

    Refactor app logo into one file

--- src/core/components/AppLogo.js
+++ src/core/components/AppLogo.js
@@ -1,16 +1,32 @@
 import React from 'react';
-import {Link} from 'react-router';
-import AppLogoIcon from './AppLogoIcon';
-import AppLogoName from './AppLogoName';
-import classNames from '../styles/AppLogoStylesheet.css';
+import {H1, Link} from 'shared/components/Typography';
+import {LogoIcon, LogoNameIcon} from 'shared/components/Icons';
+import classnames from 'classnames/bind';
+import classNamesContext from '../styles/AppLogoStylesheet.css';
+
+const cx = classnames.bind(classNamesContext);
+
+const AppLogoIcon = (props, context) => <div className={cx({logoIcon: true})}>
+  <LogoIcon
+    className={cx({icon: true, faded: !context.loggedIn})}
+    width={52}
+    height={52}
+  />
+</div>;
+
+const AppLogoName = _ => <div className={cx({logoName: true})}>
+  <H1 className={cx({appTitle: true})}>
+    <LogoNameIcon width={120} height={'auto'} />
+  </H1>
+</div>;
 
 const AppLogo = (props, context) => {
   const {router} = context;
   if (router.isActive('/', true)) {
-    return <div className={classNames.this} />;
+    return <div className={cx({this: true})} />;
   }
   return <Link
-    className={classNames.this}
+    className={cx({this: true})}
     to={'/'}
     onlyActiveOnIndex
   >
@@ -19,6 +35,9 @@ const AppLogo = (props, context) => {
   </Link>;
 };
 
+AppLogoIcon.contextTypes = {
+  loggedIn: React.PropTypes.bool,
+};
 AppLogo.contextTypes = {
   router: React.PropTypes.object,
 };
